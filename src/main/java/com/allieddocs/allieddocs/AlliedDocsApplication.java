7e2841fee4e7ace5e2de8fdb2c514e7f81354154
Anup Saha <anup12596@gmail.com>
2022-12-24 04:08:04 -0700

    Init Commit

---
+++ src/main/java/com/allieddocs/allieddocs/AlliedDocsApplication.java
@@ -0,0 +1,13 @@
+package com.allieddocs.allieddocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlliedDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlliedDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allieddocs/allieddocs/AlliedDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allieddocs.allieddocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlliedDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
